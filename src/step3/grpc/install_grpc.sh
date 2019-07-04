ced45527669e3b81f2f24916011fabad33d9b7c6
He Dengfeng <hedengfeng@lingtrack.com>
2019-07-04 18:43:50 +0800

    2019.7.04 learing grpc

---
+++ src/step3/grpc/install_grpc.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/grpc/grpc-go.git E:\GOPATH/src/google.golang.org/grpc
+git clone https://github.com/golang/net.git E:\GOPATH/src/golang.org/x/net
+git clone https://github.com/golang/text.git E:\GOPATH/src/golang.org/x/text
+git clone https://github.com/google/go-genproto.git E:\GOPATH/src/google.golang.org/genproto
+git clone https://github.com/golang/tools.git E:\GOPATH/src/golang.org/x/tools
--- src/step3/protobuf/hello_client.go
+++
@@ -1,8 +0,0 @@
-package main
-import (
-	"github.com/golang/protobuf/proto"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/status"
-)
